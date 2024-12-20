3147eda94a529323559f8ebf9d8c1cadbdf8c34f
timur <ageevtimur83@yandex.ru>
2024-12-20 20:16:51 +0300

    1 ну ты сам знаешь

---
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class RectangleArea {
+    public static double square(double p, double k) {
+
+        double result = (Math.pow(p, 2) * k) / Math.pow(2 * k + 2, 2);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        double result = RectangleArea.square(6, 2);
+        System.out.println(result);
+    }
+}
