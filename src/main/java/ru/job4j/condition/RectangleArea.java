34edeb741f6278afb94d26916b3104c2922aa0cf
timur <ageevtimur83@yandex.ru>
2024-12-20 20:17:22 +0300

    1 ну ты сам знаешь

--- src/main/java/ru/job4j/condition/RectangleArea.java
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -8,7 +8,7 @@ public class RectangleArea {
     }
 
     public static void main(String[] args) {
-        double result = RectangleArea.square(6, 2);
+        double result = RectangleArea.square(12, 2);
         System.out.println(result);
     }
 }
