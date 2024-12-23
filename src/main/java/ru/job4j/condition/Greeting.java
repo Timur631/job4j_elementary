24d7a5d5fa3834dffdfc31a035e6de4bf301ed80
timur <ageevtimur83@yandex.ru>
2024-12-23 16:46:01 +0300

    1 ну ты сам знаешь

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like java";
+        String ideaa = " But i am a newbie ";
+        int year = 2024;
+        String id = idea + ideaa + year;
+        System.out.println(id);
+    }
+}
